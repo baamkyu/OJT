f4c7750390c1dfc4b2985d8f6ba6c061c74be9f6
baamkyu <baamkyu@gmail.com>
2023-12-14 17:58:03 +0900

    update

--- 6/OJT6/src/components/login/GoogleLogin.tsx
+++ 6/OJT6/src/components/login/GoogleLogin.tsx
@@ -1,29 +1,16 @@
-import {
-  GoogleLogin,
-  GoogleOAuthProvider,
-  // useGoogleLogin,
-  // hasGrantedAnyScopeGoogle,
-} from "@react-oauth/google";
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
-import { useSetAtom } from "jotai";
-
-import { isLoginAtom } from "../../store/store";
 
 /** 구글로그인 시도하는 함수 */
 const TryGoogleLogin = () => {
-  const setIsLogin = useSetAtom(isLoginAtom);
   const navigate = useNavigate();
 
   const clientId =
     "22782167732-u6d2fmv2bnoh4k6jkmj6nmkkal9hcavq.apps.googleusercontent.com";
 
-  // const hasAccess = hasGrantedAnyScopeGoogle(tokenResponse, "profile");
-
-  // useGoogleLogin({ scope: "name" });
-
   const onSuccess = (res) => {
     console.log(res);
-    setIsLogin(true);
+    localStorage.setItem("token", "success");
     navigate("/");
   };
 
--- 6/OJT6/src/components/project/ContentBox.tsx
+++ 6/OJT6/src/components/project/ContentBox.tsx
@@ -8,6 +8,7 @@ import {
 import { useAtomValue, useSetAtom } from "jotai";
 
 import OJT5 from "./OJT5";
+import { useEffect } from "react";
 
 const ContentBox = () => {
   const selectedProject = useAtomValue(selectedProjectAtom);
@@ -16,111 +17,62 @@ const ContentBox = () => {
   const setOjt3 = useSetAtom(ojt3Atom);
   const setOjt4 = useSetAtom(ojt4Atom);
 
-  const ojt1CorrectNum = () => {
-    setOjt1((prevOjt1) => ({
-      ...prevOjt1,
-      correctNum: prevOjt1.correctNum + 1,
-    }));
-  };
-  const ojt1WrongNum = () => {
-    setOjt1((prevOjt1) => ({
-      ...prevOjt1,
-      wrongNum: prevOjt1.wrongNum + 1,
-    }));
-  };
-  const ojt2TotalTime = (time) => {
-    setOjt2((prevOjt2) => ({
-      ...prevOjt2,
-      finishedNum: prevOjt2.finishedNum + 1,
-      time: prevOjt2.time + time,
-    }));
-  };
-  const ojt3statistics = (getMoney, hitRate, hitBullet, hitBomb, hitMoney) => {
-    setOjt3((prevOjt3) => ({
-      ...prevOjt3,
-      getMoney: prevOjt3.getMoney + getMoney,
-      hitRate: prevOjt3.hitRate + hitRate,
-      hitBullet: prevOjt3.hitBullet + hitBullet,
-      hitBomb: prevOjt3.hitBomb + hitBomb,
-      hitMoney: prevOjt3.hitMoney + hitMoney,
-    }));
-  };
-
-  const iframe = () => {
-    window.addEventListener("message", function (event) {
+  useEffect(() => {
+    const handler = function (event: MessageEvent) {
       switch (event.data.type) {
         case "ojt1 correct":
-          ojt1CorrectNum();
+          setOjt1((prevOjt1) => ({
+            ...prevOjt1,
+            correctNum: prevOjt1.correctNum + 1,
+          }));
           console.log("ojt1 correct 실행");
           break;
         case "ojt1 wrong":
-          ojt1WrongNum();
+          setOjt1((prevOjt1) => ({
+            ...prevOjt1,
+            wrongNum: prevOjt1.wrongNum + 1,
+          }));
           console.log("ojt1 wrong 실행");
           break;
         case "ojt2-1 finish":
           console.log("ojt2-1 finish 실행");
           break;
         case "ojt2-2 finish":
-          ojt2TotalTime(event.data.time);
+          setOjt2((prevOjt2) => ({
+            ...prevOjt2,
+            finishedNum: prevOjt2.finishedNum + 1,
+            time: prevOjt2.time + event.data.time,
+          }));
           console.log("ojt2-2 finish 실행");
           break;
         case "ojt3 finish":
           console.log("ojt3 finish 실행");
-          ojt3statistics(
-            event.data.getMoney,
-            event.data.hitRate,
-            event.data.hitBullet,
-            event.data.hitBomb,
-            event.data.hitMoney
-          );
+          setOjt3((prevOjt3) => ({
+            ...prevOjt3,
+            getMoney: prevOjt3.getMoney + event.data.getMoney,
+            hitRate: prevOjt3.hitRate + event.data.hitRate,
+            hitBullet: prevOjt3.hitBullet + event.data.hitBullet,
+            hitBomb: prevOjt3.hitBomb + event.data.hitBomb,
+            hitMoney: prevOjt3.hitMoney + event.data.hitMoney,
+          }));
           break;
         case "ojt4 finish":
           console.log("ojt4 finish 실행");
           break;
       }
-    });
+    };
+    window.addEventListener("message", handler);
+    return () => window.removeEventListener("message", handler);
+  }, [setOjt1, setOjt2, setOjt3]);
 
-    switch (selectedProject) {
-      case "1":
-        return (
-          <iframe
-            src="/projects/ojt1/dist/index.html"
-            width="100%"
-            height="100%"
-            className="shadow-lg drop-shadow-l"
-          />
-        );
-      case "2":
-        return (
-          <iframe
-            src="/projects/ojt2/dist/index.html"
-            width="100%"
-            height="100%"
-            className="shadow-lg drop-shadow-l"
-          />
-        );
-
-      case "3":
-        return (
-          <iframe
-            src="/projects/ojt3/dist/index.html"
-            width="100%"
-            height="100%"
-          />
-        );
-      case "4":
-        return (
-          <iframe
-            src="/projects/ojt4/dist/index.html"
-            width="100%"
-            height="100%"
-          />
-        );
-      case "5":
-        return <OJT5 />;
-    }
-  };
-
-  return <>{iframe()}</>;
+  if (!selectedProject) return null;
+  if (selectedProject === "5") return <OJT5 />;
+  return (
+    <iframe
+      src={`/projects/ojt${selectedProject}/dist/index.html`}
+      width="100%"
+      height="100%"
+    />
+  );
 };
 export default ContentBox;
